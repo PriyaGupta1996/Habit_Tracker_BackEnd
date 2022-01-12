0eb02bda77a353c5c7d86574871ab7262bef45ad
Priya Gupta <priyagupta.ynr@gmail.com>
2022-01-12 20:03:26 +0530

    habit route creation

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/ping', (req, res) => {
-  res.send("pong");
+router.get('/habit', (req, res) => {
+  res.json({ habit: 1, name: "calls" });
 }
 );
 
